062c2f01dd526f882d69de0f7ec8836bc8a9db30
hingolfur <hingolfur2@gmail.com>
2022-03-05 15:59:26 +0000

    test v.1.0

---
+++ tol401g_assignment11/MyAssignment11.java
@@ -0,0 +1,16 @@
+// You are free to modify this class, e.g. add fields, methods,
+// constructors, extend from class Thread etc. with the two exceptions:
+// 1. Do not change the name of this class nor put it into another package
+// 2. Do not modify the name and input and output parameter of the main method.
+
+public class MyAssignment11 { // TODO: Modify as necessary
+
+	// TODO: fill in your code here
+
+	public static long main(long iterationsPerThread) { // Do not modify this line!
+
+		// TODO: fill in your code here
+
+	}
+
+}
